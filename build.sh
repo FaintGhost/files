4fc4baceb53ea4f8128a094bc44323945be78672
西行寺鬼鬼子 <zhang.yaowei@live.com>
2021-07-10 23:02:35 +0200

    update

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/FaintGhost/files/raw/main/Dockerfile
-wget https://github.com/FaintGhost/files/raw/main/System.Net.Http.dll
-wget https://github.com/FaintGhost/files/raw/main/Emby.Web.dll
-wget https://raw.githubusercontent.com/FaintGhost/files/main/embypremiere.js
-docker build -f Dockerfile -t ttdark/emby_crack:latest .
